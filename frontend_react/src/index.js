7c83870b2d0bcbbeb618a965103a3ef878ee858a
Corey Hellwege <coreyhellwege@gmail.com>
2022-03-20 15:36:59 +1100

    Install React and create container components

---
+++ frontend_react/src/index.js
@@ -0,0 +1,7 @@
+import react from 'react'
+import reactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+
+// Render the entire App within the 'root' div in index.html
+reactDOM.render(<App />, document.getElementById('root'))
