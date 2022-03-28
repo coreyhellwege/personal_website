85b274bce36eb74fb4df6240b78a15bb284e0d25
Corey Hellwege <coreyhellwege@gmail.com>
2022-03-28 11:06:38 +1100

    Create Social Media and Nav Dots componenets

--- frontend_react/src/components/index.js
+++ frontend_react/src/components/index.js
@@ -1,2 +1,4 @@
 // Create aliases for each component for cleaner importing into App.js
 export { default as Navbar } from './Navbar/Navbar'
+export { default as NavigationDots } from './NavigationDots'
+export { default as SocialMedia } from './SocialMedia'
