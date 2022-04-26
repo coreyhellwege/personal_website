be05b3e0200ddc35e530456de1c88383469152e6
Corey Hellwege <coreyhellwege@gmail.com>
2022-04-26 22:28:11 +1000

    Start building light/dark mode switch

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,19 +1,46 @@
 import React from 'react'
+import { createContext, useState } from 'react'
+import ReactSwitch from 'react-switch'
+import { BsLightningChargeFill, BsMoonFill } from 'react-icons/bs'
 import { Folio, Footer, Header, TechStack, Experience, Education, Projects } from './container'
 import { Navbar } from './components'
 import './App.scss'
 
+export const ThemeContext = createContext(null) // default context is null
+
 const App = () => {
-    return <div className='app'>
-        <Navbar />
-        <Header />
-        <Projects />
-        <TechStack />
-        <Experience />
-        <Folio />
-        <Education />
-        <Footer />
-    </div>
+    const [theme, setTheme] = useState('light')
+
+    const toggleTheme = () => {
+        setTheme((current) => (current === 'light' ? 'dark' : 'light'))
+    }
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            <div className='app' id={theme}>
+                <Navbar />
+                <ReactSwitch 
+                    className='app__switch'
+                    onChange={toggleTheme} 
+                    checked={theme === 'dark'} 
+                    uncheckedIcon={<BsLightningChargeFill className='app__switch-icons light' />}
+                    checkedIcon={<BsMoonFill className='app__switch-icons dark' />}
+                    offColor='#fff'
+                    onColor='#e4e4e4'
+                    offHandleColor='#eef3f8'
+                    onHandleColor='#fff'
+                    handleDiameter={20}
+                />
+                <Header />
+                <Projects />
+                <TechStack />
+                <Experience />
+                <Folio />
+                <Education />
+                <Footer />
+            </div>
+        </ThemeContext.Provider>
+    )
 }
 
 export default App
