1f2f98c93f9e549ac9457b3ba108ad8f9d3c1f77
Corey Hellwege <coreyhellwege@gmail.com>
2022-05-01 11:51:51 +1000

    Move ReactSwitch into the Navbar component

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,6 +1,4 @@
 import React, { createContext, useState } from 'react'
-import ReactSwitch from 'react-switch'
-import { BsSunFill, BsMoonFill } from 'react-icons/bs'
 import { Folio, Footer, Header, TechStack, Experience, Education, Projects } from './container'
 import { Navbar } from './components'
 import './App.scss'
@@ -9,27 +7,12 @@ export const ThemeContext = createContext(null) // default context is null
 
 const App = () => {
     const [theme, setTheme] = useState('light')
-
-    const toggleTheme = () => {
-        setTheme((current) => (current === 'light' ? 'dark' : 'light'))
-    }
+    const toggleTheme = () => setTheme((current) => (current === 'light' ? 'dark' : 'light'))
 
     return (
         <ThemeContext.Provider value={{ theme, toggleTheme }}>
             <div className='app' id={theme}>
-                <Navbar />
-                <ReactSwitch 
-                    className='app__switch'
-                    onChange={toggleTheme} 
-                    checked={theme === 'dark'} 
-                    uncheckedIcon={<BsSunFill className='app__switch-icons light' />}
-                    checkedIcon={<BsMoonFill className='app__switch-icons dark' />}
-                    offColor='#fff'
-                    onColor='#302d29'
-                    offHandleColor='#eef3f8'
-                    onHandleColor='#1E1B18'
-                    handleDiameter={20}
-                />
+                <Navbar theme={theme} toggleTheme={toggleTheme} />
                 <Header theme={theme} />
                 <Projects />
                 <TechStack />
--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,29 @@
 import React, { useState } from 'react'
+import ReactSwitch from 'react-switch'
+import { BsSunFill, BsMoonFill } from 'react-icons/bs'
 import { HiMenuAlt4, HiX } from 'react-icons/hi'
 import { motion } from 'framer-motion'
 import { images } from '../../constants'
 import './Navbar.scss'
 
-const Navbar = () => {
-    const [toggle, setToggle] = useState(false)
+const Navbar = ({ theme, toggleTheme }) => {
+    const [menuToggle, setMenuToggle] = useState(false)
 
     return <nav className='app__navbar'>
-        <div className='app__navbar-logo'>
+        <div className='app__navbar-logo-section'>
             <img src={images.logo} alt='logo' />
+            <ReactSwitch 
+                className='app__theme-switch'
+                onChange={toggleTheme} 
+                checked={theme === 'dark'} 
+                uncheckedIcon={<BsSunFill className='app__theme-switch-icons light' />}
+                checkedIcon={<BsMoonFill className='app__theme-switch-icons dark' />}
+                offColor='#fff'
+                onColor='#302d29'
+                offHandleColor='#eef3f8'
+                onHandleColor='#1E1B18'
+                handleDiameter={20}
+            />
         </div>
         <ul className='app__navbar-links'>
             {['home', 'projects', 'tech stack', 'experience', 'design', 'education', 'contact'].map((item) => (
@@ -20,14 +34,14 @@ const Navbar = () => {
             ))}
         </ul>
         <div className='app__navbar-menu'>
-            <HiMenuAlt4 onClick={() => setToggle(true)} />
-            {toggle && (
+            <HiMenuAlt4 onClick={() => setMenuToggle(true)} />
+            {menuToggle && (
                 <motion.div whileInView={{ x: [300, 0] }} transition={{ duration: 0.85, ease: 'easeOut' }}>
-                    <HiX onClick={() => setToggle(false)} />
+                    <HiX onClick={() => setMenuToggle(false)} />
                     <ul>
                         {['home', 'projects', 'tech stack', 'experience', 'design', 'education', 'contact'].map((item) => (
                             <li key={item}>
-                                <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
+                                <a href={`#${item}`} onClick={() => setMenuToggle(false)}>{item}</a>
                             </li>
                         ))}
                     </ul>
