8474f2ceb505b544103a08bc0b85a268bbd07393
Corey Hellwege <coreyhellwege@gmail.com>
2022-06-13 19:32:26 +1000

    Optionally render project images

--- frontend_react/src/container/Projects/Projects.jsx
+++ frontend_react/src/container/Projects/Projects.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { AiFillEye, AiFillGithub } from 'react-icons/ai'
+import { AiFillGithub } from 'react-icons/ai'
 import { motion } from 'framer-motion'
 import { AppWrap, MotionWrap } from '../../wrapper'
 import { urlFor, client } from '../../client'
@@ -29,7 +29,7 @@ const Projects = () => {
 
         setTimeout(() => {
             setAnimateCard([{ y: 0, opacity: 1 }]) // re-trigger cards shuffle animation when category changes
-            item === 'All' ? setFilterProject(projects) : setFilterProject(projects.filter(project => project.tags.includes(item)))
+            item === 'All' ? setFilterProject(projects) : setFilterProject(projects.filter(project => project.tags && project.tags.includes(item)))
         }, 500);
     }
 
@@ -41,7 +41,7 @@ const Projects = () => {
             </p>
         </div>
         <div className='app__project-filter'>
-            {['React JS', 'Node JS', 'Mongo DB', 'Express JS', 'Next JS', 'Sanity', 'All'].map((item, index) => (
+            {['React JS', 'Node JS', 'Mongo DB', 'Express JS', 'Next JS', 'TypeScript', 'All'].map((item, index) => (
                 <div 
                     key={index} 
                     onClick={() => handleProjectFilter(item)}
@@ -58,23 +58,13 @@ const Projects = () => {
         >
             {filterProject.map((project, index) => (
                 <div className='app__project-item app__flex' key={index}>
-                    <div className='app__project-img app__flex'>
+                    {project.imgUrl && <div className='app__project-img app__flex'>
                         <img src={urlFor(project.imgUrl)} alt={project.name} />
                         <motion.div
                             whileHover={{ opacity: [0, 1] }}
                             transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
                             className='app__project-hover app__flex'
                         >
-                            {/* <a href={project.projectLink} target='_blank' rel='noreferrer'>
-                                <motion.div
-                                    whileInView={{ scale: [0, 1] }}
-                                    whileHover={{ scale: [1, 0.9] }}
-                                    transition={{ duration: 0.25 }}
-                                    className='app__flex'
-                                >
-                                    <AiFillEye />
-                                </motion.div>
-                            </a> */}
                             <a href={project.codeLink} target='_blank' rel='noreferrer'>
                                 <motion.div
                                     whileInView={{ scale: [0, 1] }}
@@ -86,9 +76,11 @@ const Projects = () => {
                                 </motion.div>
                             </a>
                         </motion.div>
-                    </div>
+                    </div>}
                     <div className='app__project-content app__flex'>
-                        <h4>{project.title}</h4>
+                        <a href={project.codeLink} target='_blank' rel='noreferrer'>
+                            <h4>{project.title}</h4>
+                        </a>
                         <p className='p-text' style={{ marginTop: 10 }}>{project.description}</p>
                         <div className='app__project-tag app__flex'>
                             <p className='p-text'>{project.year}</p>
